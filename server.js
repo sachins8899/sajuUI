c9c3c14aec2c5f830016619835fd7e6d87e6d1c3
Sachin Saini <sachin.saini8899@gmail.com>
2019-11-12 18:00:50 +0530

    Changes prod env in json file-3

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const path = require('path');
 
 const app = express();
 
+const fs = require('fs');
+
+
+fs.readdir(__dirname, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log("We are Here : - "+file); 
+    });
+});
+
 console.log('Jai Ho Mata Di:'+ __dirname);
 
 // Serve only the static files form the dist directory
