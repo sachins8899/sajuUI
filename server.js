0732a591b6ad086a446eca302054f55ce36c3266
Sachin Saini <sachin.saini8899@gmail.com>
2019-11-12 18:26:39 +0530

    Changes prod env in json file-4

--- server.js
+++ server.js
@@ -4,29 +4,23 @@ const path = require('path');
 
 const app = express();
 
-const fs = require('fs');
-
-
-fs.readdir(__dirname, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log("We are Here : - "+file); 
-    });
-});
-
 console.log('Jai Ho Mata Di:'+ __dirname);
 
+const fs = require('fs');
+
+fs.readdir(__dirname, (err, files) => {
+  files.forEach(file => {
+    console.log("ko - "+file);
+  });
+});
+
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'dist/sajuui'));
+app.use(express.static(__dirname + '/dist/sajuUI'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/sajuui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/sajuUI/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
